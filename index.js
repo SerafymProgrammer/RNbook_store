5443085fedf065565153f3deec083724db6b0c79
SerafymProgrammer <malishevserafym@gmail.com>
2019-08-06 10:20:02 +0300

    change index js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-/**
- * @format
- */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import React from 'react';
 
-AppRegistry.registerComponent(appName, () => App);
+import { name as appName } from './app.json';
+import { Provider } from 'react-redux';
+
+import store from './src/configureStore';
+import AppContainer from './src/App';
+
+// const store = configureStore()
+
+const RNRedux = () => (
+  <Provider store = { store }>
+    <AppContainer />
+  </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => RNRedux);
